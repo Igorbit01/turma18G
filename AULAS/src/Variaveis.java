5c8104ddf98cc12fe23428aca515bf691b48f5dc
igorbit01 <igor.barbosaduarte@gmail.com/>
2021-02-24 18:24:37 -0300

    Arquivos desenvolvidos em portugol

---
+++ AULAS/src/Variaveis.java
@@ -0,0 +1,12 @@
+
+public class Variaveis {
+	public static void main(String[] args) {
+		int idade;	
+		double salario;
+		String nome;
+		byte 
+		floOat 
+		
+	}
+
+}
---
+++ AULAS/src/nomes.java
@@ -0,0 +1,9 @@
+
+public class nomes {
+
+	public static void main(String[] args) {
+		System.out.println(" Igor Augusto Barbosa Duarte ");
+		System.out.println(" Marcia luzia Barbosa ");
+		System.out.println("Adielson de Oliveira Duarte");
+}
+}
